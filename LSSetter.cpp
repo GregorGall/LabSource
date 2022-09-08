b72f5f93191f31e94edaa2dad91a2df3ee4ca46c
GregorGall <GregorGall50@gmail.com>
2022-09-09 00:33:19 +0300

    LabSource control start

---
+++ LSSetter.cpp
@@ -0,0 +1,108 @@
+/*###################################################################################################################*/
+
+#include "LabSource.h"
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка частоты:
+
+auto LabSource::setFrequency(double frequency) -> LabSource& {
+    
+    LabSource::frequency = frequency;
+    modelDots();
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка множителя частоты:
+
+auto LabSource::setMult(double mult) -> LabSource&
+{
+    LabSource::mult = mult;
+    modelDots();
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка начальной фазы:
+
+auto LabSource::setPhase(double phase) -> LabSource&
+{
+    LabSource::phase = phase;
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка временных границ:
+
+auto LabSource::setTime(double start, double stop) -> LabSource&
+{
+    LabSource::start = start;
+    LabSource::stop = stop;
+    modelDots();
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка амплитуды:
+
+auto LabSource::setAmplitude(double amplitude) -> LabSource&
+{
+    LabSource::amplitude = amplitude;
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка временного дискрета:
+
+auto LabSource::setTimeDiscrete(double discrete) -> LabSource&
+{
+    LabSource::timeDiscrete = discrete;
+    sampling = false;
+    modelDots();
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка режима дискретизации (auto/self):
+
+auto LabSource::AutoSampling(bool mode) -> LabSource&
+{
+    try {
+        if (timeDiscrete <= 0) { throw "Error: auto_sampling mode = true with timeDiscret <= 0 \n"; }
+        sampling = mode;
+    }
+    catch (std::string& error) {
+        std::cout << error;
+    }
+    
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка количества точек моделируемых на одно колебание:
+
+auto LabSource::setDotsPerOsc(int dotsPerOsc) -> LabSource&
+{
+    LabSource::dotsPerOsc = dotsPerOsc;
+    modelDots();
+
+    return *this;
+}
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+// Установка параметров модуляции:
+
+auto LabSource::setModParameters(ModParameters modParameters) -> LabSource&
+{
+    LabSource::modParameters = modParameters;
+
+    return *this;
+}
+
+/*===================================================================================================================*/
---
+++ LabSource.h
@@ -0,0 +1,69 @@
+/*###################################################################################################################*/
+// Класс генератор
+
+#pragma once
+
+#include "../DMath/DMath.h"
+#include "LSModulation.h"
+
+/*###################################################################################################################*/
+
+class LabSource:public DMath
+{
+    
+    using field = std::vector<std::vector<double>>;
+
+private:
+
+    double stop;                                              // Начальный момент времени (запись в аргумент now + end)
+    double start;                                             // Конечный момент времени
+    double timeDiscrete;                                      // Временной дискрет
+
+    double frequency;                                         // Частота
+    double mult = 1;                                          // Множитель частоты
+    double phase = 0;                                         // Начальная фаза
+    double amplitude = 1;                                     // Амплитуда
+
+    int dotsPerOsc = 64;                                      // Количество точек моделируемых на единицу частоты
+    int dotsPerModel;                                         // Количество точек моделируемых за один вызов
+    bool sampling{true};                                      // Режим расчета временного дискрета
+    ModParameters modParameters{};                            // Параметры модуляции
+
+public:
+
+    LabSource(double start, double stop, double frequency);   // Конструктор (начальное время, конечное время, частота)
+
+    auto sin()                              -> LabSource&;    // Синусоида
+    auto level(double  level)               -> LabSource&;    // Уровень
+    auto sin(ModParameters modPr)           -> LabSource&;    // Модулированная синусоида
+    auto ambguity(double lFreq,
+                  double rFreq,
+                  double step)              -> field;         // Функция неопределенности сигнала
+
+    auto clear()                            -> LabSource&;    // Очистка вывода
+    auto modelDots()                        -> void;          // Расчет количества моделируемых точек
+
+    static auto ambguity(double lFreq,
+                         double rFreq,
+                         double step,
+                         LabSource& source1,
+                         LabSource& source2) -> field;         // Функция неопределенности сигнала
+
+/*-------------------------------------------------------------------------------------------------------------------*/
+
+    auto setFrequency(double frequency)     -> LabSource&;
+    auto setPhase(double phase)             -> LabSource&;
+    auto setMult(double mult)               -> LabSource&;
+    auto setTime(double start, double stop) -> LabSource&;
+    auto setAmplitude(double amplitude)     -> LabSource&;
+    auto setTimeDiscrete(double discrete)   -> LabSource&;
+    auto setDotsPerOsc(int dotsPerOsc)      -> LabSource&;
+    auto setModParameters(ModParameters)    -> LabSource&;
+    auto AutoSampling(bool mode)            -> LabSource&;
+
+    double getTimeDiscrete() { return timeDiscrete; }
+
+};
+
+/*===================================================================================================================*/
+
